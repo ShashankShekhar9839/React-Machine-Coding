fe33af73c37588f298b112667e0eb7e38331ff96
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-09-23 12:49:48 +0530

    added feature of adding a employee

--- employee-management-system/src/App.tsx
+++ employee-management-system/src/App.tsx
@@ -8,13 +8,77 @@ import AddEmployeeContainer from "./components/AddEmployeeContainer.js";
 
 function App() {
   const { employees, setEmployees } = useContext(EmployeeContext);
+  const [addNewEmployee, setAddNewEmployee] = useState(false);
   useEffect(() => {
     console.log(employees);
   }, [employees]);
+  const handleAddNewEmployee = () => {
+    setAddNewEmployee(true);
+  };
+  const handleClose = () => {
+    setAddNewEmployee(false);
+  };
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "1px solid red",
+        padding: "30px",
+      }}
+    >
+      <button
+        onClick={handleAddNewEmployee}
+        style={{
+          alignSelf: "flex-start",
+          marginLeft: "80px",
+          background: "red",
+          border: "none",
+          padding: "10px",
+          color: "white",
+          fontWeight: "bolder",
+          cursor: "pointer",
+          textAlign: "center",
+          borderRadius: "6px",
+        }}
+      >
+        Add New Employee
+      </button>
       <EmployeeList />
-      <AddEmployeeContainer />
+      {addNewEmployee && (
+        <div
+          style={{
+            height: "auto",
+            position: "fixed",
+            padding: "20px",
+            background: "green",
+            top: "20px",
+          }}
+        >
+          <span
+            onClick={handleClose}
+            style={{
+              height: "40px",
+              width: "40px",
+              borderRadius: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "white",
+              background: "black",
+              position: "absolute",
+              right: "0",
+              top: "-20px",
+            }}
+          >
+            x
+          </span>
+          <AddEmployeeContainer />
+        </div>
+      )}
     </div>
   );
 }
